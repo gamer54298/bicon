1f6f1473e7963fea6ba852528e781865e5f01fd3
Devyn <24640365+gamer54298@users.noreply.github.com>
2020-06-29 11:07:37 -0700

    Add files via upload

---
+++ bicon.py
@@ -0,0 +1,12 @@
+print("Hit enter to obtain your number")
+input()
+#cls
+
+#RNG between 1 - 255
+import random
+for x in range(1):
+    random = random.randint(1,255)
+    print("Your number is", random + ".")
+    print("Convert your number into binary")
+    
+input()
